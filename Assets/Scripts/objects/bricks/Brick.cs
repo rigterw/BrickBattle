666ac6c28e70bf7c0ec2ec4e3fd869ce8b002b9f
rigterw <rigterwessel@gmail.com>
2023-02-10 23:33:49 +0100

    adds the brick object

--- Assets/Scripts/objects/bricks/Brick.cs
+++ Assets/Scripts/objects/bricks/Brick.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class Brick : MonoBehaviour
 {
+    BrickManager manager;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,12 +13,22 @@ public class Brick : MonoBehaviour
     }
 
     public void Init(BrickManager manager, Color color){
-        
+        this.manager = manager;
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        sprite.color = color;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /// <summary>
+    /// destroys this gameObject
+    /// </summary>
+    private void Destroy(){
+        manager.Remove(gameObject);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other){
+        if(!other.gameObject.CompareTag("ball"))return;
+
+        Destroy();
     }
 }
--- Assets/Scripts/objects/bricks/BrickManager.cs
+++ Assets/Scripts/objects/bricks/BrickManager.cs
@@ -8,6 +8,7 @@ public class BrickManager : MonoBehaviour
     [SerializeField] private GameObject brick;
     [SerializeField] private int rows;
     [SerializeField] private int collums;
+    [SerializeField] private Color[] rowColors;
 
     private List<GameObject> p1Bricks = new List<GameObject>();
     private List<GameObject> p2Bricks = new List<GameObject>();
@@ -17,14 +18,34 @@ public class BrickManager : MonoBehaviour
     }
 
     private void CreateGrid(){
-        p1Bricks.Add(Instantiate(brick));
-        Debug.Log(p1Bricks.Count());
-        Destroy(p1Bricks[0]);
+        p1Bricks.Add(SpawnBrick(0,0));
+    }
+
+    /// <summary>
+    /// Creates a new brick object
+    /// </summary>
+    /// <param name="r">the row of the brick  </param>
+    /// <param name="c">the colum of the brick</param>
+    private GameObject SpawnBrick(int r, int c, bool top = false){
+        GameObject brickObject = Instantiate(brick);
+        brickObject.GetComponent<Brick>().Init(this, rowColors[r]);
+
+        return brickObject;
+    }
+
+    public void Remove(GameObject brick){
+        if(p1Bricks.Contains(brick)){
+            p1Bricks.Remove(brick);
+        }else if(p2Bricks.Contains(brick)){
+            p2Bricks.Remove(brick);
+        }else{
+            Debug.LogError("brickmanager got passed a brick that wasn't in any list");
+            return;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(p1Bricks[0]);
     }
 }

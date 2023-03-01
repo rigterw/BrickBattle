1e36886dacf241e92faecf4a2f047a597f75a4c8
rigterw <rigterwessel@gmail.com>
2023-03-01 14:09:56 +0100

    adds ball velocity

--- Assets/Scripts/objects/Ball/BallMovement.cs
+++ Assets/Scripts/objects/Ball/BallMovement.cs
@@ -5,37 +5,66 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float acceleration;
     Vector3 Velocity;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Velocity = new Vector3(1, speed);
+        Velocity = new Vector3(0, speed);
+
+        if(acceleration <= 1){
+            Debug.LogError("invalid ball acceleration value");
+            acceleration = 1.25f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Velocity.normalized);
         rb.velocity = Velocity;
     }
 
     void OnCollisionEnter2D(Collision2D col){
         Vector3 collisionNormal = col.contacts[0].normal;
-        if(col.gameObject.CompareTag("Player"))
-            PlayerBounce(collisionNormal);
-        else
-            wallBounce(collisionNormal);
+        bool topDownCollision = Mathf.Abs(collisionNormal.y) > Mathf.Abs(collisionNormal.x);
+
+        if(col.gameObject.CompareTag("Player") && topDownCollision)
+            PlayerBounce(col);
+        else{
+            if(Velocity.magnitude > speed )
+            Velocity /= acceleration;
+            wallBounce(topDownCollision);
+        }
     }
 
-    private void PlayerBounce(Vector3 cNormal){
+    /// <summary>
+    /// function that calculates the speed if the ball bounces on a player
+    /// </summary>
+    /// <param name="player"></param>
+    private void PlayerBounce(Collision2D player){
         float speed = Velocity.magnitude;
-        wallBounce(cNormal);
+
+        float maxDistance = player.gameObject.GetComponent<SpriteRenderer>().size.x/2;
+
+
+        float xdir = transform.position.x - player.transform.position.x;
+        float midDisttance = Mathf.Abs(xdir);
+        float ydir = Mathf.Sqrt(1 - midDisttance * midDisttance);
+
+        ydir = Velocity.y >= 0 ? -1 : 1;
+        speed *= acceleration;
+
+
+
+        Velocity = new Vector2(xdir, ydir) * speed;
+
+
     }
 
-    private void wallBounce(Vector3 cNormal){
-        if(Mathf.Abs(cNormal.y)> Mathf.Abs(cNormal.x))
+    private void wallBounce(bool tdCollision){
+        if(tdCollision)
             Velocity.y = -Velocity.y;
         else
             Velocity.x = -Velocity.x;
